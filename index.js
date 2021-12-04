62e8e80e21e119e0225500675fd521c3f2bf7210
dongmin7208 <sayg3o3d@gmail.com>
2021-12-04 23:09:32 +0900

    ADD: mousemove - js

---
+++ index.js
@@ -0,0 +1,18 @@
+
+new Vue({
+    el:"#app",
+    data: {
+        number:0,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        countUp: function(times){
+            this.number += 1 * times
+        },
+        changeMousePosition: function(event, divideNumber) {
+            this.x = event.clientX / divideNumber;
+            this.y = event.clientY / divideNumber;
+        }
+    }
+})
