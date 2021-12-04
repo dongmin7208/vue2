71825fff697f1e2ba64641e95efcc310b83e41e1
dongmin7208 <sayg3o3d@gmail.com>
2021-12-05 01:27:30 +0900

    ADD: function

--- computed/computed.js
+++ computed/computed.js
@@ -2,5 +2,10 @@ new Vue({
     el:'#app',
     data: {
         counter: 0
+    },
+    computed: {
+        lessThanThree: function(){
+            return this.counter > 3 ? '3より上' : '３以下'
+        }
     }
 })
