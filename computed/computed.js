b384a17008a7883096df4019dfadf005ec5dbdf3
dongmin7208 <sayg3o3d@gmail.com>
2021-12-07 22:18:58 +0900

    ADD: setTimeout

--- computed/computed.js
+++ computed/computed.js
@@ -7,5 +7,13 @@ new Vue({
         lessThanThree: function(){
             return this.counter > 3 ? '3より上' : '３以下'
         }
+    },
+    watch: {
+        counter: function(){
+            var vm = this;
+            setTimeout(function(){
+
+            },3000)
+        }
     }
 })
