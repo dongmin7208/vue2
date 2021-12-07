298cfb635e2475195d2764cf6ee87ec8758579d5
dongmin7208 <sayg3o3d@gmail.com>
2021-12-07 22:28:09 +0900

    ADD: ()

---
+++ parenthesis/parenthesis.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: "#app",
+    data: {
+        counter: 0
+    },
+    computed: {
+        doubleCounterComputed: function() {
+            return this.counter * 2;
+        }
+    },
+    methods: {
+        countUp: function() {
+            this.counter += 1;
+        },
+        doubleCounterMethod: function(){
+            return this.counter * 2;
+        }
+    }
+})
