275990433c2787bdd1cb64ac4ea8f31f25d787dd
dongmin7208 <sayg3o3d@gmail.com>
2021-12-24 19:02:08 +0900

    reactivity

--- reactivity/reactivity.js
+++ reactivity/reactivity.js
@@ -1,32 +1,12 @@
+var data = {
+    message: 'こんにちは',
+    name: 'どんどん'
+}
+
 var vm =new Vue({
     el:"#app",
-    data: {
-     message: 'hello',
-     firstName: 'park',
-     lastName: 'dongmin'
-    },
-    computed: {
-        fullName: {
-          get () {
-            alert("Assembling Full Name ... ");
-            return this.firstName + ' ' + this.lastName;
-          },
-          set (newValue) {
-            alert("Setting new name: " + newValue);
-            const parts = newValue.split(' ');
-            this.firstName = parts[0];
-            this.lastName = parts[parts.length - 1];
-          }
-        }
-      },
-      methods: {
-        // ...
-        changeNameSetter: function(newName) {
-          this.fullName = newName; 
-        }
-      },
+    data: data
 })
-// vm.message= ' change!'
-// vm.message = 'change'
-// vm.name = 'park'
+
+console.log(data === vm.$data);
 console.log(vm)
